061930b4263dd29173bc4384cd61fae3daa5a6ef
Alan Barr <freedom480@gmail.com>
2020-05-12 16:11:02 -0700

    Added open image logic to image viewer app.

--- ImageViewerApp/MainWindow.xaml.cs
+++ ImageViewerApp/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace ImageViewerApp
 {
@@ -10,6 +14,48 @@ namespace ImageViewerApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_OpenImage_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Title = "Select an image to open",
+                FileName = "",
+                DefaultExt = ".bmp",
+                Filter = "Image files (*.bmp/jpg/png/tif)|*.bmp;*.jpg;*.png;*.tif",
+                Multiselect = false,
+                CheckFileExists = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                tbImageName.Text = openFileDialog.FileName;
+
+                UpdateImage(openFileDialog.FileName);
+            }
+        }
+
+        private void UpdateImage(string filename)
+        {
+            UpdateImage(Image.FromFile(filename));
+        }
+
+        private void UpdateImage(Image image)
+        {
+            if (pbImage.CheckAccess())
+            {
+                // Convert to WPF image
+                ImageSource imageSource = ImageConverter.ConvertImageToBitmap(image);
+
+                // Update GUI with image
+                pbImage.Source = imageSource;
+            }
+            else
+            {
+                pbImage.Dispatcher.BeginInvoke((Action)delegate () { UpdateImage(image); } );
+            }
         }
     }
 }
