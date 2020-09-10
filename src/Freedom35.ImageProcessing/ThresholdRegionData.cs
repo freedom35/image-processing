a389593773374ef4e1eb2dbc7e4f842d1d2a60d6
Alan Barr <freedom480@gmail.com>
2020-09-10 12:58:06 -0700

    Added property comments.

--- src/Freedom35.ImageProcessing/ThresholdRegionData.cs
+++ src/Freedom35.ImageProcessing/ThresholdRegionData.cs
@@ -11,42 +11,65 @@ namespace Freedom35.ImageProcessing
     /// </summary>
     internal struct ThresholdRegionData
     {
+        /// <summary>
+        /// Distance calculated from region.
+        /// </summary>
         public double Distance
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Threshold value of region.
+        /// </summary>
         public byte Threshold
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// X position of region.
+        /// </summary>
         public int X
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Y position of region.
+        /// </summary>
         public int Y
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Horizontal center of region.
+        /// </summary>
         public int CenterX
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Vertical center of image.
+        /// </summary>
         public int CenterY
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Calculates distance of x,y from region.
+        /// </summary>
+        /// <param name="x">X position of pixel</param>
+        /// <param name="y">Y position of pixel</param>
         public void CalculateDistance(int x, int y)
         {
             Distance = Math.Sqrt(Math.Pow(Math.Abs(x - CenterX), 2) + Math.Pow(Math.Abs(y - CenterY), 2));
