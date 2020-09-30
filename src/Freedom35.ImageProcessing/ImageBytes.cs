4da52deb107550416aab2ed598207ada5bb75f99
Alan Barr <freedom480@gmail.com>
2020-09-29 17:34:11 -0700

    Consolodated ImageEncoding into ImageBytes class.

--- src/Freedom35.ImageProcessing/ImageBytes.cs
+++ src/Freedom35.ImageProcessing/ImageBytes.cs
@@ -3,6 +3,7 @@
 // License: MIT
 //------------------------------------------------
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -420,6 +421,57 @@ namespace Freedom35.ImageProcessing
             }
 
             return byteValues;
+        }
+
+        /// <summary>
+        /// Determines image type based on header bytes.
+        /// </summary>
+        /// <param name="imageBytes">Image bytes</param>
+        /// <param name="imageType">Type of image determined</param>
+        /// <returns>True if image type could be determined</returns>
+        public static bool TryGetImageType(byte[] imageBytes, out ImageType imageType)
+        {
+            imageType = ImageType.Unknown;
+
+            // Get image types to compare
+            IEnumerable<ImageType> imageTypes = Enum.GetValues(typeof(ImageType)).Cast<ImageType>().Where(t => t != ImageType.Unknown);
+
+            // Decode image type in buffer
+            foreach (ImageType type in imageTypes)
+            {
+                byte[] encodingBytes = type.GetEncodingBytes();
+
+                if (IsImageType(imageBytes, encodingBytes))
+                {
+                    imageType = type;
+                    break;
+                }
+            }
+
+            return imageType != ImageType.Unknown;
+        }
+
+        /// <summary>
+        /// Determines if image matches the encoding type.
+        /// </summary>
+        /// <param name="imageBytes">Image bytes</param>
+        /// <param name="encodingBytes">Encoding bytes for comparison</param>
+        /// <returns>True if image header matches encoding bytes</returns>
+        public static bool IsImageType(byte[] imageBytes, byte[] encodingBytes)
+        {
+            int i;
+
+            // Compare image bytes to encoding
+            for (i = 0; i < encodingBytes.Length && i < imageBytes.Length; i++)
+            {
+                if (imageBytes[i] != encodingBytes[i])
+                {
+                    break;
+                }
+            }
+
+            // Check if all encoding bytes matched buffer
+            return (i == encodingBytes.Length);
         }
     }
 }
