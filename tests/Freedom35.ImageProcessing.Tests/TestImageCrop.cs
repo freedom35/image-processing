6194dbf7288e772011df8afd4fc19c4d8d59c880
Alan Barr <freedom480@gmail.com>
2020-10-02 19:31:00 -0700

    Added unit test for crop by region.

---
+++ tests/Freedom35.ImageProcessing.Tests/TestImageCrop.cs
@@ -0,0 +1,47 @@
+using Freedom35.ImageProcessing;
+using Freedom35.ImageProcessing.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace ImageProcessingTests
+{
+    [TestClass]
+    public class TestImageCrop
+    {
+        [DataRow("Freedom35.ImageProcessing.Tests.Resources.clock.bmp")]
+        [DataRow("Freedom35.ImageProcessing.Tests.Resources.clock.jpg")]
+        [DataRow("Freedom35.ImageProcessing.Tests.Resources.clock.png")]
+        [DataRow("Freedom35.ImageProcessing.Tests.Resources.clock.tif")]
+        [DataTestMethod]
+        public void TestByRegion(string resourcePath)
+        {
+            // Load source image
+            using Image sourceImage = TestImage.FromResource(resourcePath);
+
+            Assert.IsNotNull(sourceImage);
+
+            Rectangle region = new Rectangle(0, 0, sourceImage.Width / 2, sourceImage.Height / 2);
+
+            // Crop region of image
+            using Image croppedImage = ImageCrop.ByRegion(sourceImage, region);
+
+            Assert.IsNotNull(croppedImage);
+
+            // Check size
+            Assert.AreEqual(region.Width, croppedImage.Width);
+            Assert.AreEqual(region.Height, croppedImage.Height);
+
+            // Get bytes for each image
+            byte[] sourceBytes = ImageBytes.FromImage(sourceImage);
+            byte[] croppedBytes = ImageBytes.FromImage(sourceImage);
+
+            // Compare to ensure correct region cropped
+            int limit = croppedImage.Width * croppedImage.Height;
+
+            for (int i = 0; i < limit; i++)
+            {
+                Assert.AreEqual(sourceBytes[i], croppedBytes[i]);
+            }
+        }
+    }
+}
