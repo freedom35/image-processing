360e555b8907fe4d3eebb27b095e990ad1b4777f
Alan Barr <freedom480@gmail.com>
2020-10-02 19:21:01 -0700

    Added unit tests for convolution enum.

---
+++ tests/Freedom35.ImageProcessing.Tests/TestConvolutionTypeEnum.cs
@@ -0,0 +1,37 @@
+using Freedom35.ImageProcessing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using Freedom35.ImageProcessing.Tests;
+
+namespace ImageProcessingTests
+{
+    [TestClass]
+    public class TestConvolutionTypeEnum
+    {
+        [DataRow(ConvolutionType.Edge, "Edge")]
+        [DataRow(ConvolutionType.EdgeSobelVertical, "Sobel Vertical Edge")]
+        [DataTestMethod]
+        public void TestGetDescription(ConvolutionType type, string description)
+        {
+            Assert.AreEqual(type.GetDescription(), description);
+        }
+
+        [TestMethod]
+        public void TestGetConvolutionMatrix()
+        {
+            ConvolutionType type = ConvolutionType.Smoothing;
+
+            // 2D matrix
+            int[,] matrix = type.GetConvolutionMatrix();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    // Smoothing filter contains all 1's
+                    Assert.AreEqual(1, matrix.GetValue(i, j));
+                }
+            }
+        }
+    }
+}
